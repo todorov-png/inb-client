211d72542f614af38cd684d1aa8afe7106f4a641
Evgeniy <zhenya.todorov@gmail.com>
2023-04-30 15:13:26 +0300

    fix

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from '@vue/cli-service';
 export default defineConfig({
   transpileDependencies: true,
   publicPath:
-    process.env.NODE_ENV === 'production' ? `${process.env.BASE_URL}:${process.env.PORT}` : '/',
+    process.env.NODE_ENV === 'production' ? `${process.env.BASE_URL}` : '/',
 });
 
